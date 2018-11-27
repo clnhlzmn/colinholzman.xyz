3578b2d79e895b20b0b9899cd0fa53242c744e16
Colin Holzman <colinholzman@Colins-MacBook-Pro.local>
2018-11-27 18:39:13 -0500

    this time

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ const privateKey = fs.readFileSync('/etc/letsencrypt/live/colinholzman.xyz/privk
 const certificate = fs.readFileSync('/etc/letsencrypt/live/colinholzman.xyz/cert.pem', 'utf8');
 const ca = fs.readFileSync('/etc/letsencrypt/live/colinholzman.xyz/chain.pem', 'utf8');
 
-const index = fs.readFileSync(path.join(__dirname, 'index.html'));
-
 const credentials = {
     key: privateKey,
     cert: certificate,
@@ -29,7 +27,7 @@ const credentials = {
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 
